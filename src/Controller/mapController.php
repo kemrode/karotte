e802063277146d17db3d4c29f49cc126dc1ffcec
LeauHic <loickbrouard@yahoo.fr>
2021-03-26 10:34:24 +0100

    add giignore

--- src/Controller/mapController.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace src\Controller;
-class MapController extends AbstractController {
-
-    public function index(){
-        return $this->twig->render("map/mapView.html.twig");
-    }
-}
