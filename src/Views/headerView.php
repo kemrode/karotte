3c9bbf343fc7879a78617a156d114c79338ad8b3
Kemrode <kevinfichou@gmail.com>
2021-05-05 21:50:11 +0200

    added package.json OK, but not working on headerView.php

---
+++ src/Views/headerView.php
@@ -0,0 +1,21 @@
+<?php
+?>
+
+<script src="https://code.iconify.design/1/1.0.6/iconify.min.js"></script>
+<link rel="stylesheet" href="/public/assets/css/sass/Layout/_headerStyle.scss">
+<header>
+    <div class="tabBarMenuContainer tabBarMenu__backgound">
+        <div class="burgerMenu">
+            <a href="/" class="iconify" data-inline="false" data-icon="cil:hamburger-menu" style="color: #6b8954; font-size: 35px;"></a>
+        </div>
+        <div class="homeMenu">
+            <a href="/" class="iconify" data-inline="false" data-icon="ant-design:home-filled" style="color: #bb4e0b; font-size: 30px;"></a>
+        </div>
+        <div class="basketMenu">
+            <a href="/" class="iconify" data-inline="false" data-icon="fa:shopping-basket" style="color: #bb4e0b; font-size: 30px;"></a>
+        </div>
+        <div class="myAccount">
+            <a href="/" class="iconify" data-inline="false" data-icon="carbon:user-avatar-filled" style="color: #bb4e0b; font-size: 30px;"></a>
+        </div>
+    </div>
+</header>
