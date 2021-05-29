2dbd2d88a02b504fbac3b3b21c1aca4e392c903d
Kemrode <kevinfichou@gmail.com>
2021-05-29 14:01:34 +0200

    <integration>: commit before pull

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -3,7 +3,8 @@ namespace src\Controller;
 
 use src\Model\SellerModel;
 
-Class ProfileController extends AbstractController{
+Class
+ProfileController extends AbstractController{
     function index(){
         $sellerList = SellerModel::GetAllSellers();
         return $this->twig->render("profile/ProfileSeller.html.twig",["sellerList"=>$sellerList]);
