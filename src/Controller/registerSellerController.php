743b9b1ffc24fae8b8c9b482ee2655c3d20e4bbd
Kemrode <kevinfichou@gmail.com>
2021-05-31 23:21:43 +0200

    <feat>registerSeller : back to main view after register new seller

--- src/Controller/registerSellerController.php
+++ src/Controller/registerSellerController.php
@@ -17,26 +17,22 @@ class registerSellerController extends AbstractController {
         $sellerItems = ['nom','adresse','code postale','ville'];
         return $this->twig->render('register/registerSellerView.html.twig', ['sellerItems'=>$sellerItems]);
     }
-
     public function addNewSeller(){
         try {
             if (isset($_POST['okButton'])){
                 $newSellerId = $_SESSION['userId'];
                 $newSellerAdress = userModel::GetCoordinatesFromAdress($_POST['adresse'], $_POST['zipCode'], $_POST['ville']);
-                //$sellerAdressToPost = explode(';',$newSellerAdress);
                 $newSeller = new SellerModel();
                 $newSeller->setSELLID($newSellerId);
                 $newSeller->setSELLNAME($_POST['nom']);
                 $newSeller->setSELLLOC($newSellerAdress);
                 $newSeller->setSELLPRES($_POST['presentation']);
                 $newSellerToPost = $newSeller->postNewSeller(BDD::getInstance());
-                header('Loation:/');
+                header('Location:/');
             }
-
         } catch(\Exception $e){
-            var_dump($e);
+            throw $e;
         }
-
     }
 
 
