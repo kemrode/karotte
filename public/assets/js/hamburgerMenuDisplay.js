ad2b5625e994dc89a98449d11c45005f4f34ee43
Kemrode <kevinfichou@gmail.com>
2021-06-12 16:51:43 +0200

    <feat>(mediaQuerries): header view responsiv OK

---
+++ public/assets/js/hamburgerMenuDisplay.js
@@ -0,0 +1,24 @@
+//let blockToDisplay = document.getElementsByClassName("myAccountBtn");
+//let contentDrop = document.getElementsByClassName(".dropContent");
+let blockToDisplay = document.querySelectorAll(".minSize");
+let contentDrop = document.querySelectorAll(".dropMinSize");
+console.log(blockToDisplay);
+//function to display hamburgerMenu when media max-width 768px
+//member icone:
+blockToDisplay[1].addEventListener('click', ()=>{
+    if(contentDrop[1].style.display == "block"){
+        contentDrop[1].style.display = "none";
+    } else {
+        contentDrop[1].style.display = "block";
+        contentDrop[1].style.marginTop = "200px";
+    }
+});
+//hamburgermenu icon:
+blockToDisplay[0].addEventListener('click', ()=>{
+    if(contentDrop[0].style.display == "block"){
+        contentDrop[0].style.display = "none";
+    } else {
+        contentDrop[0].style.display = "block";
+        contentDrop[0].style.marginTop = "251px";
+    }
+});
